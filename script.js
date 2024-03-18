98af0782dc43b71cd833ad0aa62762dcc4ef23da
mohamedgad00 <gad254530@gmail.com>
2024-03-18 14:55:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'block';
+})
